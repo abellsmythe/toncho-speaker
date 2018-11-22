a9f1c8ed8da5204623ace919b5711e1d6e80690c
{Alton Bell Smythe} <abellsmythe@gmail.com>
2018-11-22 01:23:07 -0300

    Toncho Speaker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    voices: [],
+    text: "hello world",
+    voice: "",
+    display: false
+  };
+
+  synth = window.speechSynthesis;
+
+  handleClickLogo = e => {
+    e.preventDefault();
+    this.setState({
+      display: !this.state.display,
+      voices: this.synth.getVoices()
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const voice = this.state.voices.find(
+      voice => voice.name === this.state.voice
+    );
+    const utterThis = new SpeechSynthesisUtterance(this.state.text);
+    utterThis.voice = voice;
+
+    this.synth.speak(utterThis);
+  };
+
+  handleChangeText = e => {
+    e.preventDefault();
+    this.setState({
+      text: e.currentTarget.value
+    });
+  };
+
+  handleChangeSelect = e => {
+    e.preventDefault();
+    this.setState({
+      voice: e.currentTarget.value
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <section className="container">
+          <img
+            src={logo}
+            className="logo"
+            alt="logo"
+            style={{ height: this.state.display ? "25vmin" : "50vmin" }}
+            onClick={this.handleClickLogo}
+          />
+          <p>Toncho Speaker</p>
+          {this.state.display && (
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                placeholder="Text"
+                value={this.state.text}
+                onChange={this.handleChangeText}
+              />
+              <select onChange={this.handleChangeSelect}>
+                {this.state.voices &&
+                  this.state.voices.map(voice => (
+                    <option key={voice.name} value={voice.name}>
+                      {voice.lang}
+                    </option>
+                  ))}
+              </select>
+              <input type="submit" value="Speak!" />
+            </form>
+          )}
+        </section>
       </div>
     );
   }
